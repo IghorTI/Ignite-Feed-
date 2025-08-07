561fdc66a005aebc8bbb6a69b8d88d1f3d3150c2
Ighor Rodrigues de Carvalho <ighorrcarvalho.ti@gmail.com>
2025-08-07 17:05:36 -0300

    feat: Post HTML

---
+++ src/components/Post.jsx
@@ -0,0 +1,36 @@
+import styles from './Post.module.css'
+
+
+export function Post(){
+
+    return(
+       <article className={styles.post}>
+
+        <header>
+            <div className={styles.author}>
+
+                <img
+                    className={styles.avatar} 
+                    src="https://github.com/IghorTI.png" 
+                />
+
+                <div className={styles.authorInfo}>
+                    <strong>Ighor Rodrigues</strong>
+                    <span>FullStack Developer</span>
+                </div>
+            </div>
+
+            <time title='7 de Agosto de 2025 as 16h58' dateTime='2025-08-07 16:57:30'>Publicado há 1h</time>
+        </header>
+
+        <div className={styles.content}>
+            <p>Fala galeraaa</p>
+            <p>Acabei de subir mais um projeto parao GitHub. É um projeto do Ignite da Rocketseate</p>
+            <p><a href=''> IghorTI</a></p>
+            <p><a href=''>#novoprojeto #ignite</a></p>
+        </div>
+
+       </article>
+    )
+    
+}
