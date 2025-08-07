7027834daa884d0110e1c0113e604a98e50bd135
Ighor Rodrigues de Carvalho <ighorrcarvalho.ti@gmail.com>
2025-08-07 16:34:04 -0300

    feat: Finished Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import {PencilLine} from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar(){
@@ -8,12 +10,19 @@ export function Sidebar(){
                 src='https://images.unsplash.com/photo-1519241047957-be31d7379a5d?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
             />
             <div className={styles.profile}>
+
+                <img 
+                    className={styles.avatar}
+                    src='https://github.com/IghorTI.png'
+                />
+
                 <strong>Ighor Rodrigues</strong>
                 <span>Fullstack Developer</span>
             </div>
 
             <footer>
                 <a href='#'>
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
